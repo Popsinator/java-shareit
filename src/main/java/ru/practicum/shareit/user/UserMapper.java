2ca8b238703edf0529d5f4b1188cf1f2b3787857
Popsinator <pop.alekseika@yandex.ru>
2022-10-25 10:18:03 +0300

    fix: UserMapper - убрал комментарии

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -2,23 +2,24 @@ package ru.practicum.shareit.user;
 
 public class UserMapper {
 
-    public UserDto toUserDtoWithoutName(User user) {//Dto для обновления email
+    public UserDto toUserDtoWithoutName(User user) {
         UserDto userDto = new UserDto();
         userDto.setEmail(user.getEmail());
         userDto.setId(user.getId());
         return userDto;
     }
 
-    public UserDto toUserDtoWithoutEmail(User user) {//Dto для обновления name
+    public UserDto toUserDtoWithoutEmail(User user) {
         UserDto userDto = new UserDto();
         userDto.setName(user.getName());
         userDto.setId(user.getId());
         return userDto;
     }
 
-    public User toDtoUserWithoutEmail(UserDto userDto) {//Создание пользователя из Dto
+    public User toDtoUserWithoutEmail(UserDto userDto) {
         return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
+
     public User toDtoUserWithoutName(UserDto userDto) {
         return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
